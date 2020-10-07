fe466492a8bf39d33df8baac8a01402b033f5f37
下田眞人 <1711140059s@ed.fuk.kindai.ac.jp>
2020-10-07 06:38:15 +0000

    演習1

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,19 @@
+class TopController < ApplicationController
+  def main
+    if session[:login_uid]
+      render :main
+    else
+      render :login
+    end
+  end
+
+  def login
+      if params[:uid] =='kindai' and params[:pass] == 'sanriko'
+        session[:login_uid] = params[:uid]
+        redirect_to '/'
+      else
+        render :error
+      end
+  end
+  
+end
